5064c3f0f5c7e7d90840806a778d13c1cb906ac5
002657351_SonalGurav <sgurav621@gmail.com>
2022-11-16 21:39:02 -0800

    Exercise1

---
+++ Exercise1/src/FullTime.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class FullTime extends Student{
+    private double examScore1;
+    private double examScore2;
+
+    public FullTime(String firstName, String lastName, String country, HashMap<String, Integer> quizScores, double examScore1, double examScore2) {
+        super(firstName, lastName, country, quizScores);
+        this.examScore1 = examScore1;
+        this.examScore2 = examScore2;
+    }
+
+    public double getExamScore1() {
+        return examScore1;
+    }
+
+    public void setExamScore1(double examScore1) {
+        this.examScore1 = examScore1;
+    }
+
+    public double getExamScore2() {
+        return examScore2;
+    }
+
+    public void setExamScore2(double examScore2) {
+        this.examScore2 = examScore2;
+    }
+}
---
+++ Exercise1/src/PartTime.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class PartTime extends Student{
+
+
+    public PartTime(String firstName, String lastName, String country, HashMap<String, Integer> quizScores) {
+        super(firstName, lastName, country, quizScores);
+    }
+}
