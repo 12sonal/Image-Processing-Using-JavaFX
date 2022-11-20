91ad47d607abbf79f70c0bdc9a8be364eda69a9b
Sonal Gurav <30283399+12sonal@users.noreply.github.com>
2022-11-19 22:49:41 -0800

    Re-uploaded

--- Exercise1/src/PartTime.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-
-public class PartTime extends Student{
-
-
-    public PartTime(String firstName, String lastName, String country, HashMap<String, Integer> quizScores) {
-        super(firstName, lastName, country, quizScores);
-    }
-}
