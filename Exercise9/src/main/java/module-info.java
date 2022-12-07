19776010f7ddb97be4f9c9a26496a388136ec7b5
Sonal Gurav <30283399+12sonal@users.noreply.github.com>
2022-12-06 21:49:24 -0800

    Delete Exercise9/src/main/java directory

--- Exercise9/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.exercise9 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-
-    opens Model.exercise9 to javafx.fxml;
-    exports Model.exercise9;
-}
